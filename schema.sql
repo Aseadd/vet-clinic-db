85804ff41c3c9866ce0eeb48a030ee0dd34bcf4d
Addis Tsega <48018471+Aseadd@users.noreply.github.com>
2022-10-18 02:53:29 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(
-    id INT, 
-    name VARCHAR(250),
-    date_of_birth DATE,
-    escape_attempts INT, 
-    neutered BOOLEAN, 
-    wight_kg DECIMAL
-    );
+
