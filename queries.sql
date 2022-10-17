1243c44b3306201e5c3ca5139eb8dc722586176d
Addis Tsega <48018471+Aseadd@users.noreply.github.com>
2022-10-18 02:25:49 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE neutered = true AND escape_attempts <3;
+SELECT name, escape_attempts FROM animals WHERE wight_kg >10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+SELECT * FROM animals WHERE wight_kg >=10.4 AND wight_kg <=17.3;
