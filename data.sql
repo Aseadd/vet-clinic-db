1947e0629f6aa7b2eb5ab3d48c998fffeec213a4
Addis Tsega <48018471+Aseadd@users.noreply.github.com>
2022-10-19 21:43:46 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -7,4 +7,5 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, wight_kg) VALUES ('Charmander', '2020/02/08', 0, false, -11), 
 ('Plantmon','2021/03/15', 2, true, -5.7), ('Squirtle', '1993/04/02', 3, false, -12.13), ('Angemon', '2005/06/12', 1, true, -45), 
 ('Boarmon','2005/06/07', 7, true, 20.4), ('Blossom', '1998/10/13', 3, true, 17), ('Ditto', '2022/05/14', 4, true, 22);
-
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
