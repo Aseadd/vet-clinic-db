c76db878cc5ebe6cc8cb09e31d659fbd56b86499
addis <betsega23@gmail.com>
2022-10-21 01:34:32 +0300

    Add schema

--- schema.sql
+++ schema.sql
@@ -16,4 +16,18 @@ ALTER TABLE animals ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERE
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners (id);
 
+CREATE TABLE vets (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), age INT, date_of_graduation DATE, PRIMARY KEY(id));
+
+CREATE TABLE spevialization (species_id INT, vets_id INT, PRIMARY KEY (species_id, vets_id),
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id) 
+);
+CREATE TABLE visits( animal_id INT, vets_id INT,visiting_date DATE,
+  PRIMARY KEY (animal_id, vets_id, visiting_date),
+  CONSTRAINT fk_animals FOREIGN KEY(animal_id) REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+
+
 
